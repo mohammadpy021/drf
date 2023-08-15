e90fbff9b997f5175bb88ce4917e0f948c7880ab
mohammadpy021 <mohammad.py021@gmail.com>
2023-08-15 15:25:31 +0330

    Dynamic Field Mixin(Third Party)

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,9 @@
 from blog.models import Blog
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-
+from drf_dynamic_fields import DynamicFieldsMixin
 class UserSerializer(serializers.ModelSerializer): 
-    # def to_representation(self, obj):   #first 1
+    # def to_representation(self, obj):   
     #     return obj.username
 
     class Meta:
@@ -14,14 +14,14 @@ class UserSerializer(serializers.ModelSerializer):
 
         
 
-class ArticleSerializer(serializers.ModelSerializer):
-    # author = UserSerializer() # depend on fields or to_representation
-    # author = UserSerializer(read_only = True)  # depend on fields or to_representation
-    # author = serializers.CharField(source='author.username', read_only=True) #second 2
+class ArticleSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
+    # author = UserSerializer()                     # depends on "fields" in UserSerializer or to_representation
+    # author = UserSerializer(read_only = True)     # depends on "fields" in UserSerializer or to_representation
+    # author = serializers.CharField(source='author.username', read_only=True) 
     author_url = serializers.HyperlinkedIdentityField(view_name='api:users-detail')
-    author = serializers.SerializerMethodField()# third 3
-    def get_author(self, obj):# third 3
-        return obj.author.username
+    author = serializers.SerializerMethodField()
+    def get_author(self, obj):
+        return obj.author.username # or return {"username": obj.author.username, "first_name" : "obj.author.first_name"}
     class Meta:
         model = Blog
         fields = "__all__"
@@ -30,18 +30,20 @@ class ArticleSerializer(serializers.ModelSerializer):
         if value.lower()  in ['django', 'admin']:
             raise serializers.ValidationError("this word is forbiden")
         return value
-    # set current user as author
-    def create(self, validated_data): 
-        request = self.context.get('request', None)
-        user = Blog.objects.create(author= request.user, **validated_data)
-        return user
-    # make readOnly the author if it is not superuser
-    def get_fields(self, *args, **kwargs):
-        fields = super().get_fields(*args, **kwargs)
-        request = self.context.get('request', None)
-        if  not request.user.is_superuser  :
-            fields['author'].read_only = True
-        return fields
+    
+    ## set current user as author
+    # def create(self, validated_data): 
+    #     request = self.context.get('request', None)
+    #     user = Blog.objects.create(author= request.user, **validated_data)
+    #     return user
+
+    ## make readOnly the "author" field if user is not superuser
+    # def get_fields(self, *args, **kwargs):
+    #     fields = super().get_fields(*args, **kwargs)
+    #     request = self.context.get('request', None)
+    #     if  not request.user.is_superuser  :
+    #         fields['author'].read_only = True
+    #     return fields
     
     
 
